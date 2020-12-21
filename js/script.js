0cb62af2b6bf360e4b31ce185388e4a93d7531a2
Artem <zzzfisherzzz@gmail.com>
2020-12-21 22:50:04 +0200

    Made second practice task

--- js/script.js
+++ js/script.js
@@ -10,13 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('Насколько вы его оцениваете?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('Насколько вы его оцениваете?', '');
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('Насколько вы его оцениваете?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && a != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель.');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
